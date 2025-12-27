3bf27975318399ae19530082ef87876a382bb0d0
Daniel Kuettel <dkuettel@gmail.com>
2025-12-27 13:46:40 +0100

    more plain

--- lua/mad-mappings/actions/plain.lua
+++ lua/mad-mappings/actions/plain.lua
@@ -67,13 +67,84 @@ local function up_fast()
     end
 end
 
+---@param d number sign is direction, fraction is relative to page size
+local function page(d)
+    -- NOTE we use this instead of ctrl-u and ctrl-d, they have problems keeping the cursor in the same column always
+    -- NOTE this calculation is off with folds, im not using them, also disabled in diff mode
+    -- TODO paging is a bit off at the top and bottom with scrolloff, but maybe that's okay?
+    return function()
+        local view = vim.fn.winsaveview()
+        local s = math.floor(d * (vim.api.nvim_win_get_height(0) - 1))
+        local h = vim.api.nvim_buf_line_count(0)
+        vim.fn.winrestview {
+            lnum = math.min(math.max(1, view.lnum + s), h),
+            topline = math.min(math.max(1, view.topline + s), h),
+            -- NOTE behavior is a bit unspecified, but this works for me so far
+            col = view.col + view.coladd,
+            coladd = 0,
+        }
+    end
+end
+
 local nv = require("mad-mappings").make_action_nv
 
 return {
-    down = nv { "cursor down visual line", expr = down },
-    down_fast = nv { "cursor and view down visual line", expr = down_fast },
-    down_adaptive = nv { "down or down_fast", expr = rapid(down, down_fast) },
-    up = nv { "cursor up visual line", expr = up },
-    up_fast = nv { "cursor and view up visual line", expr = up_fast },
-    up_adaptive = nv { "up or up_fast", expr = rapid(up, up_fast) },
+    cursor = {
+        down = {
+            plain = nv { "cursor down visual line", expr = down },
+            fast = nv { "cursor down visual line and view if past half screen", expr = down_fast },
+            adaptive = nv { "down or down_fast", expr = rapid(down, down_fast) },
+        },
+        up = {
+            plain = nv { "cursor up visual line", expr = up },
+            fast = nv { "cursor up visual line and view if past half screen", expr = up_fast },
+            adaptive = nv { "up or up_fast", expr = rapid(up, up_fast) },
+        },
+        left = nv { "cursor left", rhs = "h" },
+        right = nv { "cursor right", rhs = "l" },
+        word_start = {
+            previous = {
+                small = nv { "previous word start", rhs = "b" },
+                big = nv { "previous WORD start", rhs = "B" },
+            },
+            next = {
+                small = nv { "next word start", rhs = "w" },
+            },
+        },
+        word_end = {
+            next = {
+                small = nv { "next word end", rhs = "e" },
+                big = nv { "next WORD end", rhs = "E" },
+            },
+            previous = {
+                small = nv { "previous word end", rhs = "ge" },
+            },
+        },
+        start_of_text = nv { "cursor to start of text, view to start of line", rhs = "0^" },
+        start_of_line = nv { "cursor to start of line", rhs = "0" },
+        end_of_line = nv { "cursor to end of line", rhs = "$" },
+        page = {
+            full = {
+                up = nv { "view and cursor one page up", fn = page(-1) },
+                down = nv { "view and cursor one page down", fn = page(1) },
+            },
+            half = {
+                up = nv { "view and cursor one page up", fn = page(-0.5) },
+                down = nv { "view and cursor one page down", fn = page(0.5) },
+            },
+        },
+        first_line = nv { "cursor to first line", rhs = "gg" },
+        first_character = nv { "cursor to first character in first line", rhs = "gg0" },
+        last_line = nv { "cursor to last line", rhs = "G" },
+        last_character = nv { "cursor to last character in last line", rhs = "G$" },
+    },
+    view = {
+        down = nv { "view down", rhs = "1<c-e>" },
+        up = nv { "view up", rhs = "1<c-y>" },
+        left = nv { "view left", rhs = "z<left>" },
+        right = nv { "view right", rhs = "z<right>" },
+        centered = nv { "move view to have cursor centered vertically", rhs = "zz" },
+        topped = nv { "move view to have cursor at the top", rhs = "zt" },
+        bottomed = nv { "move view to have cursor at the bottom", rhs = "zb" },
+    },
 }
